e6779c42b76efe3dd471ed33438f7a5beaa22ec9
Dwhite4 <dwhite4@guilford.edu>
2023-12-11 19:54:09 -0500

    dsadsd

--- src/main/java/edu/guilford/QuizPanel.java
+++ src/main/java/edu/guilford/QuizPanel.java
@@ -1,12 +1,20 @@
 package edu.guilford;
 
+import java.awt.font;
 import java.awt.Dimension;
 
+
+
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class QuizPanel extends JPanel {
 
-    
+    private JLabel quizTitle;
 
 
 
@@ -17,6 +25,11 @@ public class QuizPanel extends JPanel {
 
     public void initPanel() {
         setPreferredSize(new Dimension(600,600));
-    
+
+        quizTitle = new JLabel("Chemistry Quiz");
+        quizTitle.setFont(new Font("Arial", font.BOLD, 20));
+        add(quizTitle);
+
+
     }
 }
