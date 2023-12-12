22c9fc59341020408be797885fc8ddfc35057db2
Dwhite4 <dwhite4@guilford.edu>
2023-12-11 19:35:05 -0500

    test

--- src/main/java/edu/guilford/Main.java
+++ src/main/java/edu/guilford/Main.java
@@ -1,7 +1,31 @@
 package edu.guilford;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+       JFrame quizFrame = new JFrame("Quiz Game");
+       quizFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+       JPanel xPanel = new JPanel();
+
+       QuizPanel quizPanel = new QuizPanel();
+
+       xPanel.add(quizPanel);
+       quizFrame.add(xPanel);
+       quizFrame.pack();
+       quizFrame.setVisible(true);
+
+
+       
     }
+
+
 }
+
+   
+
---
+++ src/main/java/edu/guilford/QuizPanel.java
@@ -0,0 +1,22 @@
+package edu.guilford;
+
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+public class QuizPanel extends JPanel {
+
+    
+
+
+
+    public QuizPanel(){
+        super();
+        initPanel();
+    }
+
+    public void initPanel() {
+        setPreferredSize(new Dimension(600,600));
+    
+    }
+}
